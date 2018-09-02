b47287115de8994ee858bcf3a03ebe268408be0b
Amanda Oliveira <amandinha.g.2009@hotmail.com>
2018-09-02 16:00:42 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$msg = 0;
+@$msg= $_REQUEST['msg'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Formulario</title>
+</head>
+<body>
+
+
+	<fieldset>
+		<legend><h3>Formulário de contatos</h3></legend>
+		<form action="processaForm.php" method="get">
+			<label for="nome">Nome: </label><br>
+			<input id="nome" type="text" name="nome" required><br>
+			<label for="telefone">Telefone: </label><br>
+			<input id="telefone" type="tel" name="telefone" required><br>
+			<label for="email">Email: </label><br>
+			<input id="email" type="email" name="email" required><br>
+			<label for="assunto">Menssagem</label><br>
+			<textarea name="assunto" id="assunto"></textarea><br>
+			<input  type="submit">
+		</form>
+	</fieldset>
+
+</body>
+</html>
---
+++ processaForm.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$para = "amandaoliveirabcc@gmail.com";
+$assunto = "Contato pelo Site";
+$nome = $_REQUEST['nome'];
+$fone = $_REQUEST['telefone'];
+$email = $_REQUEST['email'];
+$msg = $_REQUEST['assunto'];
+
+	$corpo = "<strong>Mensagem de Contato</strong><br><br>";
+	$corpo .="<strong>Nome: </strong> $nome";
+	$corpo .="<br><strong>Telefone: </strong> $fone";
+	$corpo .="<br><strong>Email: </strong> $email";
+	$corpo .="<br><strong>Mensagem: </strong> $msg";
+
+	$header = "From: $email Reply-to: $email";
+	$header .= "Content-Type: text/html; charset= utf-8";
+
+mail($para, $assunto, $corpo, $header);
+
+header("location:index.php?msg=enviado");
+
+
+?>
