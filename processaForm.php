1a4646a559bfc1dff7ff5f18a5775fe236b1389a
Amanda Oliveira <amandinha.g.2009@hotmail.com>
2018-09-02 16:10:10 -0300

    small update

--- processaForm.php
+++ processaForm.php
@@ -14,8 +14,9 @@ $msg = $_REQUEST['assunto'];
 	$corpo .="<br><strong>Email: </strong> $email";
 	$corpo .="<br><strong>Mensagem: </strong> $msg";
 
-	$header = "From: $email Reply-to: $email";
-	$header .= "Content-Type: text/html; charset= utf-8";
+	$header = "Content-Type: text/html; charset= utf-8\n";
+	$header .= "From: $email Reply-to: $email\n";
+	
 
 @mail($para, $assunto, $corpo, $header);
 
